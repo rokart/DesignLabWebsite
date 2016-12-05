8a54cb8e373864d947183cdbfb6d5c3ce50aa165
Rokas <rvaleckas@uk.ey.com>
2016-12-05 15:13:13 +0000

    added jquery ui easings

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
-'use strict';
-var $ = require('jQuery');
+//'use strict';
 
-$('header h1').html('Works');
+//var $ = require('jQuery'); 
+$ = window.jQuery = require("jquery");
+require("jquery-ui-browserify");
+
 
 var ScrollMagic = require('scrollmagic');
 var controller = new ScrollMagic.Controller();
@@ -20,5 +22,57 @@ var scene = new ScrollMagic.Scene({
 $('#nav-icon').click(function () {
     $(this).toggleClass('open');
     $('.whiteheader').toggleClass('open');
-    
+
 });
+
+function getPageScroll() {
+    var yScroll;
+    if (document.documentElement && document.documentElement.scrollTop) {
+        yScroll = document.documentElement.scrollTop;
+    } else if (document.body) {
+        yScroll = document.body.scrollTop;
+    }
+    return yScroll;
+}
+
+//home page first slide scroll animation
+pageheight = $(window).height();
+$(window).on("load resize", function () {
+    pageheight = $(this).height();
+});
+var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel";
+var body = $('html, body');
+$(window).bind(mousewheelevt, function (e) {
+    var evt = window.event || e;
+    evt = evt.originalEvent ? evt.originalEvent : evt;
+    var delta = evt.detail ? evt.detail * (-40) : evt.wheelDelta;
+
+    if (delta > 0 && getPageScroll() < pageheight + 120) {
+        if (body.is(':animated')) {
+            return false;
+        } else {
+            body.stop(true, false).animate({
+                scrollTop: 0
+            }, 600, "easeOutCubic");
+        }
+    } else if (getPageScroll() > -1 && getPageScroll() < pageheight) {
+        if (body.is(':animated')) {
+            return false;
+        } else {
+            body.stop(true, false).animate({
+                scrollTop: pageheight
+            }, 600, "easeOutCubic");
+        }
+    }
+});
+
+
+//home page down arrow click event and loop animation    
+$(".white_down_arrow").on("click", function () {
+    body.stop(true, false).animate({
+        scrollTop: pageheight
+    }, 600, "easeOutCubic");
+});
+(function loop() {
+    $('.white_down_arrow').delay(500).fadeTo(1000, 0.2).fadeTo(1000, 1, loop);
+})();
